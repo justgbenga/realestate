46d9d362be7a578b080324724c0db1d6dc247a55
OSINUGA OLUGBENGA DAVID <justgbenga@gmail.com>
2015-03-09 16:24:58 +0100

    first commit

---
+++ app/models/advert.server.model.js
@@ -0,0 +1,63 @@
+																																																																																																																																																								'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Advert Schema
+ */
+var AdvertSchema = new Schema({
+	region : {
+		type: String,
+		default:'Seine Maritime'
+	},
+	city : {
+		type: String,
+		default:'Le Havre'
+	},
+	postcode: {
+		type: Number,
+		default:76600
+	},
+	name: {
+		type: String,
+		default:'OSINUGA OLUGBENGA DAVID'
+	},
+	email: {
+		type: String,
+		default:'justbenga@gmail.com'
+	},
+	phone: {
+		type: Number,
+		default:44256565
+	},
+	title: {
+		type: String,
+		default:'Appart'
+	},
+	type: {
+		type: String,
+		default:'HousF3 location'
+	},
+	description: {
+		type: String,
+		default:'Beautiful house'
+	},
+	price: {
+		type: Number,
+		default:800
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Advert', AdvertSchema);
---
+++ public/modules/adverts/controllers/adverts.client.controller.js
@@ -0,0 +1,83 @@
+'use strict';
+
+// Adverts controller
+angular.module('adverts').controller('AdvertsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Adverts',
+	function($scope, $stateParams, $location, Authentication, Adverts) {
+		$scope.authentication = Authentication;
+
+		// Create new Advert
+		$scope.create = function() {
+			// Create new Advert object
+			var advert = new Adverts ({
+				name: this.name,
+				region:this.region,
+				city:this.city,
+				postcode:this.postcode,
+				email:this.email,
+				phone:this.phone,
+				title:this.title,
+				description:this.description,
+				price:this.price
+			});
+
+			// Redirect after save
+			advert.$save(function(response) {
+				$location.path('adverts/' + response._id);
+
+				// Clear form fields
+				$scope.name = '';
+				$scope.region = '';
+				$scope.city = '';
+				$scope.postcode = '';
+				$scope.email = '';
+				$scope.phone = '';
+				$scope.title = '';
+				$scope.description = '';
+				$scope.price = '';
+				
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		// Remove existing Advert
+		$scope.remove = function(advert) {
+			if ( advert ) { 
+				advert.$remove();
+
+				for (var i in $scope.adverts) {
+					if ($scope.adverts [i] === advert) {
+						$scope.adverts.splice(i, 1);
+					}
+				}
+			} else {
+				$scope.advert.$remove(function() {
+					$location.path('adverts');
+				});
+			}
+		};
+
+		// Update existing Advert
+		$scope.update = function() {
+			var advert = $scope.advert;
+
+			advert.$update(function() {
+				$location.path('adverts/' + advert._id);
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		// Find a list of Adverts
+		$scope.find = function() {
+			$scope.adverts = Adverts.query();
+		};
+
+		// Find existing Advert
+		$scope.findOne = function() {
+			$scope.advert = Adverts.get({ 
+				advertId: $stateParams.advertId
+			});
+		};
+	}
+]);
